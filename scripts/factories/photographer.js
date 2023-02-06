1afbbcc24df8445920d49a68b84ec7ded541f93c
Homarv <62882518+Homarv@users.noreply.github.com>
2023-02-06 21:50:20 +0100

    home page done

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,36 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, city, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        article.innerHTML = 
+        `
+            <a href = "./photographer.html?id=${data.id}">
+                <h2>${data.name}</h2>
+                <img src=${picture} alt=""> </img>
+            </a>
+            <h3>${city} </h3>
+            <p>${tagline} </p>
+            <p>${price}€/jour</p>
+        `;
+    /*  const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city;
+        const taglinearea = document.createElement( 'p' );
+        taglinearea.textContent = tagline;
+        const pricearea = document.createElement( 'p' );
+        pricearea.textContent = price +" €/jour";
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(taglinearea);
+        article.appendChild(pricearea); */
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, tagline, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,11 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        let photographers =  fetch("/data/photographers.json")
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        .then((photographers) => photographers.json())
+        console.log(photographers) 
+        return photographers 
     }
 
     async function displayData(photographers) {
@@ -31,7 +13,7 @@
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
+            const userCardDOM = photographerModel.getUserCardDOM(); ///compliqué cette ligne 
             photographersSection.appendChild(userCardDOM);
         });
     };
@@ -43,4 +25,3 @@
     };
     
     init();
-    
