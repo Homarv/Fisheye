bfed7e12d95674fcca9192830d7826a6dd043d88
Homarv <62882518+Homarv@users.noreply.github.com>
2023-03-21 12:00:09 +0100

    testing bug in production

--- scripts/utils/getData.js
+++ scripts/utils/getData.js
@@ -1,7 +1,7 @@
 async function getPhotographers() {
     // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
     // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-    let photographers = await fetch("/data/photographers.json")
+    let photographers = await fetch("/data/test.json")
     // et bien retourner le tableau photographers seulement une fois récupéré
     .then((x) => x.json())
     console.log(photographers)
