c0d00b47f023ee54270d92e33729763a01aed17f
Homarv <62882518+Homarv@users.noreply.github.com>
2023-03-21 13:38:59 +0100

    modify root to getData for fetch work in production and local

--- scripts/utils/getData.js
+++ scripts/utils/getData.js
@@ -1,7 +1,7 @@
 async function getPhotographers() {
     // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
     // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-    let photographers = await fetch(`${window.location.origin}${window.location.pathname}data/test.json`)
+    let photographers = await fetch(`${window.location.origin}/data/test.json`)
     // et bien retourner le tableau photographers seulement une fois récupéré
     .then((x) => x.json())
     console.log(photographers)
